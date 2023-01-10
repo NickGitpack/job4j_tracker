dcc132a4e774dab917b6be8585c7632a9d9d58dd
NickGitpack <dmitriev1616@gmail.com>
2023-01-11 01:04:50 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String unknown = "Неизвестное слово. ";
+        return unknown + eng;
+    }
+}
