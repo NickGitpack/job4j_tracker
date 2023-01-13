f4a0106d4f0d8e2ecfd76cd289d7f04de590d7f1
NickGitpack <dmitriev1616@gmail.com>
2023-01-13 21:03:40 +0300

    6.0. Абстрактные классы [#504949 #380971]

---
+++ src/main/java/ru/job4j/oop/Vehicle.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public abstract class Vehicle {
+    private String brand;
+    private String model;
+    private String type;
+    private int passengers;
+    private int maxSpeed;
+
+    public abstract void accelerate();
+
+    public abstract void steer();
+
+    public void brake() {
+        System.out.println("Стандартная тормозная система");
+    }
+}
