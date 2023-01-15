d9246c5cc2d9f2d816e1117ac11bef41d7ff4956
NickGitpack <dmitriev1616@gmail.com>
2023-01-15 16:59:05 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,31 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String NEW_VALUE = "";
+
+    String name;
+
+    Broken() {
+
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+}
