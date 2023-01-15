32b047f2328928e6a21f617d591e881e385ead24
NickGitpack <dmitriev1616@gmail.com>
2023-01-15 16:17:48 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -15,6 +18,10 @@ public class Item {
     public Item(int id, String name) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return created;
     }
 
     public int getId() {
