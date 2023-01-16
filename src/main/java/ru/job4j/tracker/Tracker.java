cba45b14c1557d1940c8b6bff9951dadbce8d228
NickGitpack <dmitriev1616@gmail.com>
2023-01-16 22:01:17 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -41,13 +41,34 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        } else if (findById(index) != null) {
+            findById(id).setName(item.getName());
+            rsl = true;
+        } else if (findById(index) == null) {
+            Item it = findById(index);
+            it = item;
+            findById(id).setName(item.getName());
+            rsl = true;
         }
         return rsl;
     }
