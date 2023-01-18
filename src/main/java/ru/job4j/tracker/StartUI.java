e09ce8c03cb985198288464defcac3b9c9a481fd
NickGitpack <dmitriev1616@gmail.com>
2023-01-18 22:08:08 +0300

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,7 +12,15 @@ public class StartUI {
             int select = Integer.parseInt(scanner.nextLine());
             if (select != 6) {
                 System.out.println("Пользователь выбрал: " + select);
-            } else {
+            }
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
