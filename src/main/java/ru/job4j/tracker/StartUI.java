b8bd3e765248685826f699320d27443a6b17b0b0
NickGitpack <dmitriev1616@gmail.com>
2023-01-19 12:23:51 +0300

    3.4. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -45,6 +45,15 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ===");
+                System.out.println("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Заявка удалена успешно.");
+                } else {
+                    System.out.println("Ошибка удаления заявки.");
+                }
             }
         }
     }
