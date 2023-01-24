d92a7d98ecdad564068cd704ae8eaed47d0a89b9
NickGitpack <dmitriev1616@gmail.com>
2023-01-24 15:24:27 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -174,4 +174,24 @@ public class StartUITest {
                 + "1. Exit" + ln
                 + "=== Exit Program ===" + ln);
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo("Menu." + ln
+                + "0. Exit" + ln
+                + "Wrong input, you can select: 0 .. 0" + ln
+                + "Menu." + ln
+                + "0. Exit" + ln
+                + "=== Exit Program ===" + ln);
+    }
 }
