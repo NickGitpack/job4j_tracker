2b4707efe25d72082d776d7eaf9f2609bfa2c709
NickGitpack <dmitriev1616@gmail.com>
2023-01-25 16:51:06 +0300

    2.1. Тест на ValidateInput [#311887 #384623]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -38,4 +38,20 @@ public class ValidateInputTest {
         assertThat(selected).isEqualTo(-1);
     }
 
+    @Test
+    public void whenFourValidInputs() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3", "4"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(3);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(4);
+    }
 }
