99ca841b87edf5d7e3b127b7b6a4d236de306029
NickGitpack <dmitriev1616@gmail.com>
2023-01-26 17:45:01 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
