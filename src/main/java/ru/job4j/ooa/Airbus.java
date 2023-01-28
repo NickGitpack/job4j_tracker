5a242f210205898972b03269c79a4e7f97008d4d
NickGitpack <dmitriev1616@gmail.com>
2023-01-28 11:24:06 +0300

    3. final [#334073 #385654]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int count = getName().equals("A320") ? 4 : COUNT_ENGINE;
+        int count = ("A380").equals(getName()) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + count);
     }
 
