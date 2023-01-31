82e7d75b258bb359f3e0e4b6db125234d8ed626f
NickGitpack <dmitriev1616@gmail.com>
2023-01-31 20:22:51 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getName().equals(key)) {
+                result.add(person);
+            } else if (person.getSurname().equals(key)) {
+                result.add(person);
+            } else if (person.getPhone().equals(key)) {
+                result.add(person);
+            } else if (person.getAddress().equals(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenNotFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Ivanov", "111111", "Lipetsk")
+        );
+        ArrayList<Person> persons = phones.find("Nikita");
+        assertThat(persons.isEmpty()).isTrue();
+    }
+}
