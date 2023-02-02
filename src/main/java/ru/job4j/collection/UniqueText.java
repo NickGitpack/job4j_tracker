a1f510f9a552d47a1c09b11ca4bbd2d8a044cdbc
NickGitpack <dmitriev1616@gmail.com>
2023-02-02 21:47:26 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+        }
+        for (String txt : text) {
+            if (!check.contains(txt)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
