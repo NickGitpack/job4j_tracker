884608b47722026351408fad1750497a0c67ec9b
NickGitpack <dmitriev1616@gmail.com>
2023-02-07 19:11:34 +0300

    3. Банковские переводы [#10038 #389073]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -20,12 +20,9 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        System.out.println(users.get(user));
-        for (Account acc : users.get(user)) {
-            System.out.println(acc.getRequisite() + " " + acc.getBalance());
-        }
-        if (!users.get(user).contains(account)) {
-            users.get(user).add(account);
+        List<Account> acc = users.get(user);
+        if (!acc.contains(account)) {
+            acc.add(account);
         }
     }
 
--- src/main/java/ru/job4j/early/Test.java
+++ src/main/java/ru/job4j/early/Test.java
@@ -1,65 +1,28 @@
 package ru.job4j.early;
 
-import static java.lang.Character.*;
+import static java.lang.Character.toLowerCase;
 
 public class Test {
-    public static boolean check(String password) {
-        boolean upper = false;
-        boolean lower = false;
-        boolean numbers = false;
-        boolean symbols = false;
-        char[] pass = password.toCharArray();
-        for (char symbol : pass) {
-            if (isUpperCase(symbol)) {
-                upper = true;
 
-            } else if (isLowerCase(symbol)) {
-                lower = true;
-            } else if (isDigit(symbol)) {
-                numbers = true;
-            } else if (symbol >= 33 && symbol <= 47
-                    || symbol >= 58 && symbol <= 64
-                    || symbol >= 91 && symbol <= 96
-                    || symbol >= 123 && symbol <= 126) {
-                symbols = true;
-            }
-        }
-        if (!upper) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-        if (!lower) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-        if (!numbers) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-        if (!symbols) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
-        return true;
-    }
-
-    public static boolean keywordsCheck(String password) {
+    public static void main(String[] args) {
+        String password = "Ln2$MUSERTY12";
+        boolean check = false;
         String[] keywords = new String[] {"qwerty", "12345", "password", "admin", "user"};
         StringBuilder sb = new StringBuilder();
         char[] pass = password.toCharArray();
         for (char index : pass) {
+            System.out.println(index);
             index = toLowerCase(index);
             sb.append(index);
+            System.out.println(index);
         }
         String str = sb.toString();
+        System.out.println(str);
         for (String keyword : keywords) {
             if (str.contains(keyword)) {
+                System.out.println(keyword);
                 throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
             }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        String password = "Ln2$mUSerTY12";
-        if (keywordsCheck(password)) {
-            System.out.println("Error");
         }
     }
 }
