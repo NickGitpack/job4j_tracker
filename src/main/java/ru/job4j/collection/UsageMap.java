27a6b62c12c607923bb7fa9d2e625e7f353c0d01
NickGitpack <dmitriev1616@gmail.com>
2023-02-07 14:54:00 +0300

    0. Map, HashMap [#212652 #388553]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("nikita@mail.ru", "Nikita Ivanov");
+        map.put("ivan@mail.ru", "Ivan Petrov");
+        map.put("vlad@mail.ru", "Vladimir Sidorov");
         for (String key : map.keySet()) {
             System.out.println(key + " = " + map.get(key));
         }
