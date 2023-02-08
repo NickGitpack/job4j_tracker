a97c7171eec372b84177a6d645b7553d6f71543a
NickGitpack <dmitriev1616@gmail.com>
2023-02-09 00:18:12 +0300

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        if (left.length() != right.length()) {
+            return Integer.compare(left.length(), right.length());
+        }
+        return 0;
+    }
+}
