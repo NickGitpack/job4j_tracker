28515ccc3d801cb2ffb2f668b50bd623bd2f1f00
NickGitpack <dmitriev1616@gmail.com>
2023-02-20 21:54:01 +0300

    2. Local-Variable Type Inference [#70626 #392890]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -9,17 +9,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 
     @Test
     public void whenNotFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
@@ -29,7 +29,7 @@ public class PhoneDictionaryTest {
         phones.add(
                 new Person("Sergey", "Ivanov", "112221", "Moscow")
         );
-        ArrayList<Person> persons = phones.find("Nikita");
+        var persons = phones.find("Nikita");
         assertThat(persons.size() == 2);
     }
 }
