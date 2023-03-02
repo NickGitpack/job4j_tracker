58c53c2bfd04a1d0929072332cabe20eb432e239
NickGitpack <dmitriev1616@gmail.com>
2023-03-02 16:30:45 +0300

    Утверждения с примитивными типами [#504883 #396462]

--- src/test/java/ru/job4j/designe/assertj/BoxTest.java
+++ src/test/java/ru/job4j/designe/assertj/BoxTest.java
@@ -36,4 +36,55 @@ class BoxTest {
                 .startsWith("Cube")
                 .isEqualTo("Cube");
     }
+
+    @Test
+    void numberOfVerticles() {
+        Box box = new Box(8, 2);
+        int amount = box.getNumberOfVertices();
+        assertThat(amount).isNotZero()
+                .isPositive()
+                .isEven()
+                .isGreaterThan(5)
+                .isLessThan(9)
+                .isEqualTo(8);
+    }
+
+    @Test
+    void numberOfVerticlesSecondTest() {
+        Box box = new Box(6, 2);
+        int amount = box.getNumberOfVertices();
+        assertThat(amount).isNotZero()
+                .isNegative()
+                .isEqualTo(-1);
+    }
+
+    @Test
+    void existTest() {
+        Box box = new Box(6, 2);
+        boolean check = box.isExist();
+        assertThat(check).isFalse();
+    }
+
+    @Test
+    void existTestSecondTest() {
+        Box box = new Box(8, 2);
+        boolean check = box.isExist();
+        assertThat(check).isTrue();
+    }
+
+    @Test
+    void getArea() {
+        Box box = new Box(8, 2);
+        assertThat(box.getArea()).isNotZero()
+                .isPositive()
+                .isGreaterThan(5)
+                .isLessThan(50)
+                .isEqualTo(24.0);
+    }
+
+    @Test
+    void getAreaSecondTest() {
+        Box box = new Box(2, 2);
+        assertThat(box.getArea()).isEqualTo(0.0);
+    }
 }
